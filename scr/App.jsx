94e11d58ad30e9f0b766c67fa820c09d431d288b
waleffsantos <38145784+waleffsantos@users.noreply.github.com>
2025-04-16 02:10:59 -0300

    Add files via upload

---
+++ scr/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Estoque de Inversores</h1>
+      <p>App base funcionando com Firebase + React + Vite</p>
+    </div>
+  )
+}
+
+export default App
